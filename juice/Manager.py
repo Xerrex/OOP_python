f90d5d7d1ab4d3ad7b6105d2e446ac84ba76b7b8
Alex Kagai <alexkagai@outlook.com>
2017-12-30 19:17:54 +0300

    code Formatting for Manager class

--- juice/Manager.py
+++ juice/Manager.py
@@ -1,23 +1,24 @@
 #! /usr/bin/python
 from .Employee import Employee
 
+
 class Manager(Employee):
 
     def __init__(self, fname, sname, pay, employees=None):
         super().__init__(fname, sname, pay)
         if employees is None:
-            self.employees =[]
+            self.employees = []
         else:
             self.employees = employees
 
-    def add_Employee(self,employee):
+    def add_Employee(self, employee):
         if employee not in self.employees:
             self.employees.append(employee)
 
-    def remove_employee(self,employee):
+    def remove_employee(self, employee):
         if employee in self.employees:
             self.employees.remove(employee)
 
     def list_employees(self):
         for employee in self.employees:
-            print('-->',employee.fullname())
+            print('-->', employee.fullname())
