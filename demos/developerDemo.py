a5c1261b58c9ece6b9a09e116a303473e526807e
Alex Kagai <alexkagai@outlook.com>
2017-12-30 19:05:52 +0300

    Add language variable for Developer creation

--- demos/developerDemo.py
+++ demos/developerDemo.py
@@ -4,8 +4,8 @@ from juice.Developer import Developer
 Demonstrates overriding of the raise amount
 '''
 
-dev1 = Developer('AlexDev', 'Kagai', 50000)
-dev2 = Developer('Alex2Dev', 'Kagai2', 60000)
+dev1 = Developer('AlexDev', 'Kagai', 50000,'python')
+dev2 = Developer('Alex2Dev', 'Kagai2', 60000,'javascript')
 
 print("\n\nraise rate:",dev1.raise_rate)
 print('\ndev1 pay:',dev1.pay)
